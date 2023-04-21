d5a9e7f035b56a0c41ba63cccc15503e99195a6d
giornn0 <shaaamsuu@gmail.com>
2023-04-21 18:59:51 -0300

    test function on template

--- resources/views/admin/tecnico/slip/index.blade.php
+++ resources/views/admin/tecnico/slip/index.blade.php
@@ -1,72 +1,73 @@
 @extends('admin.layout')
 
 @section('content')
-    <link rel="stylesheet" href="{{ asset('css/admin/tecnico/produccion_facultativo/slip_de_cotizacion.css') }}">
-    <script defer src="{{ asset('js/admin/tecnico/produccion_facultativo/slip_de_cotizacion.js') }}"></script>
-    <script defer src="{{ asset('js/admin/tecnico/produccion_facultativo/security_table.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('css/admin/tecnico/produccion_facultativo/slip_de_cotizacion.css') }}">
+<script defer src="{{ asset('js/admin/tecnico/produccion_facultativo/slip_de_cotizacion.js') }}"></script>
+<script defer src="{{ asset('js/admin/tecnico/produccion_facultativo/security_table.js') }}"></script>
 
 
-    @include('include.datatable')
+@include('include.datatable')
 
 @section('tab_title')
-    <div id="date"></div>
-    <script>
-        function updateClock() {
-            const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre',
-                'octubre', 'noviembre', 'diciembre'
-            ];
-            const days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+<div id="date"></div>
+<script>
+    function updateClock() {
+        const months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre',
+            'octubre', 'noviembre', 'diciembre'
+        ];
+        const days = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
 
-            const now = new Date();
-            const day = days[now.getDay()];
-            const date = now.getDate();
-            const month = months[now.getMonth()];
-            const year = now.getFullYear();
-            const hour = now.getHours().toString().padStart(2, '0');
-            const minute = now.getMinutes().toString().padStart(2, '0');
-            const second = now.getSeconds().toString().padStart(2, '0');
+        const now = new Date();
+        const day = days[now.getDay()];
+        const date = now.getDate();
+        const month = months[now.getMonth()];
+        const year = now.getFullYear();
+        const hour = now.getHours().toString().padStart(2, '0');
+        const minute = now.getMinutes().toString().padStart(2, '0');
+        const second = now.getSeconds().toString().padStart(2, '0');
 
-            const dateString =
-                `Producción Facultativo — Slips de Cotización | ${day}, ${date} de ${month} del ${year} ${hour}:${minute}:${second}`;
-            document.getElementById('date').textContent = dateString;
-        }
-        setInterval(updateClock, 1000);
-    </script>
+        const dateString =
+            `Producción Facultativo — Slips de Cotización | ${day}, ${date} de ${month} del ${year} ${hour}:${minute}:${second}`;
+        document.getElementById('date').textContent = dateString;
+    }
+    setInterval(updateClock, 1000);
+</script>
 @endsection
 @section('page_title')
-    Administración | Técnico
+Administración | Técnico
 @endsection
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @if (session('success'))
-    <script>
-        let timerInterval;
-        Swal.fire({
-            icon: 'success',
-            title: '¡Realizado!',
-            text: '{{ session('success') }}',
-            showConfirmButton: false,
-            timer: 4000,
-            timerProgressBar: {
-                color: '#6610f2',
-                height: '5px',
-                borderRadius: '5px',
-                opacity: '0.7'
-            },
-            didOpen: () => {
-                const b = Swal.getHtmlContainer().querySelector('.swal2-progress-bar');
-                timerInterval = setInterval(() => {
-                    const remainingTime = Swal.getTimerLeft();
-                    const progressBar = Swal.getHtmlContainer().querySelector('.swal2-progress-bar');
-                    const percent = (remainingTime / 2000) * 100;
-                    progressBar.style.width = percent + '%';
-                }, 10)
-            },
-            willClose: () => {
-                clearInterval(timerInterval);
-            }
-        });
-    </script>
+<script>
+    let timerInterval;
+    Swal.fire({
+        icon: 'success',
+        title: '¡Realizado!',
+        text: '{{ session('
+        success ') }}',
+        showConfirmButton: false,
+        timer: 4000,
+        timerProgressBar: {
+            color: '#6610f2',
+            height: '5px',
+            borderRadius: '5px',
+            opacity: '0.7'
+        },
+        didOpen: () => {
+            const b = Swal.getHtmlContainer().querySelector('.swal2-progress-bar');
+            timerInterval = setInterval(() => {
+                const remainingTime = Swal.getTimerLeft();
+                const progressBar = Swal.getHtmlContainer().querySelector('.swal2-progress-bar');
+                const percent = (remainingTime / 2000) * 100;
+                progressBar.style.width = percent + '%';
+            }, 10)
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    });
+</script>
 @endif
 
 <div id="slip" class="card" style="padding-inline: 2.5rem; padding-block: 4rem">
@@ -88,19 +89,25 @@
             </thead>
             <tbody style="overflow-y: auto; max-height: 700px;">
                 @foreach ($slipsUser as $key => $slip)
-                    <tr>
-                        <td style="text-align: center">{{ $key +1 }}</td>
-                        <td style="text-align: center">{{ $slip->insurer }}</td>
-                        <td style="text-align: center">{{ $slip->type->name }}</td>
-                        <td style="text-align: center">{{ $slip_statuses->find($slip->slip_status_id)->slip_status }}
-                        </td>
-                        <td style="text-align: center">{{ $slip->updated_at }}</td>
-                        <td style="text-align: center">
-                            <a href="{{ url('/slip_selected', [$slip->id]) }}" class="btn btn-info btn-xs"
-                                style="margin-right:16px;"> Seleccionar Slip
-                            </a>
-                        </td>
-                    </tr>
+                <tr>
+                    <td style="text-align: center">{{ $key +1 }}</td>
+                    <td style="text-align: center">{{ $slip->insurer }}</td>
+                    <td style="text-align: center">{{ $slip->type->name }}</td>
+                    <td style="text-align: center">{{ $slip_statuses->find($slip->slip_status_id)->slip_status }}
+                    </td>
+                    <td style="text-align: center">{{ $slip->updated_at }}</td>
+                    <td style="text-align: center">
+                        <a href="{{ url('/slip_selected', [$slip->id]) }}" class="btn btn-info btn-xs" style="margin-right:16px;"> Seleccionar Slip
+                        </a>
+
+                        <script>
+                            function test(msg) {
+                                console.log(msg)
+                            }
+                        </script>
+                        <button onclick="test('dsaf')">Adjuntos</button>
+                    </td>
+                </tr>
                 @endforeach
             </tbody>
         </table>
