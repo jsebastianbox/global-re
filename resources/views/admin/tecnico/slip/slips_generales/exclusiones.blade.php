c6696cbdc3fe9acaca38f20c5ab634222b46d477
Bruno Fuentes ~ br_nidas <bruno.e.fuentes97@gmail.com>
2023-04-24 11:21:45 -0300

    modificacion exclusiones form

--- resources/views/admin/tecnico/slip/slips_generales/exclusiones.blade.php
+++ resources/views/admin/tecnico/slip/slips_generales/exclusiones.blade.php
@@ -272,11 +272,11 @@
                     @break
                     @endswitch
                     <script>
-                        setTimeout(() => {
-                            const button = document.getElementById('addRowExclusionButton');
+                        $(document).ready(function () {
+                            const button = $('#addRowExclusionButton');                            
                             button.click()
+                        })
 
-                        }, 500)
                     </script>
                 </th>
             </tr>
